d29c08f94dad1e559444e39d56b8da73f8ae70aa
Jose Laruta <eduardo.laruta@gmail.com>
2023-01-01 14:07:37 -0400

    default values for env vars

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    K_SERVICE: str
-    K_REVISION: str
+    K_SERVICE: str = "mqtt_broker"
+    K_REVISION: str = "docker"
     LOG_LEVEL: str = "DEBUG"
     PORT: int = 8000
     HOST: str = "0.0.0.0"
